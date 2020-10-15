a7e5e9476c5d5d146ef847b374ca87d30eea276e
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2020-10-15 19:46:52 +0530

    Registration backend complete,moving to react front

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,11 +11,75 @@ const fetch = require('node-fetch');
 const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const expressJWT = require('express-jwt');
-// const { errorHandler } = require('../helpers/dbErrorHandling');
-const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.MAIL_KEY);
 
-exports.registerController = (req, res)=>{
-    const{name, email,password} = req.body;
-    console.log(name, email,password);
+// Custom error handler for handling error messages
+const { errorHandler } = require('../helpers/dbErrorHandling');
+
+// Using sengrid for sending mails , one can use nodemailer as well
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.MAIL_KEY);
+
+exports.registerController = (req, res) => {
+  const { name, email, password } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError
+    });
+  } else {
+    User.findOne({
+      email
+    }).exec((err, user) => {
+      // If user exists
+      if (user) {
+        return res.status(400).json({
+          errors: 'Email is taken'
+        });
+      }
+    });
+   
+    // Generate token
+    const token = jwt.sign(
+      {
+        name,
+        email,
+        password
+      },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      {
+        expiresIn: '5m'
+      }
+    );
+
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: 'Account activation link',
+      html: `
+                <h1>Please use the following to activate your account</h1>
+                <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
+                <hr />
+                <p>This email may containe sensetive information</p>
+                <p>${process.env.CLIENT_URL}</p>
+            `
+    };
+
+    sgMail
+      .send(emailData)
+      .then(sent => {
+        return res.json({
+          message: `Email has been sent to ${email}`
+        });
+      })
+      .catch(err => {
+        return res.status(400).json({
+          success: false,
+          errors: errorHandler(err)
+        });
+      });
+  }
 };
+
+// Registration Backend Complete
--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res)=>{
     });
 });
 
-const PORT =  process.env.PORT || 3000;
+const PORT =  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
